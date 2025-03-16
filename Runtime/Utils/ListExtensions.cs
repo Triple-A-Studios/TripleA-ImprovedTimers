8ed8d8f8cf4ef0e4454555ee9dc0683565b7aa85
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2025-03-16 21:59:47 +0530

    Publish Package

---
+++ Runtime/Utils/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TripleA.Utils
+{
+	public static class ListExtensions
+	{
+		/// <summary>
+		///     Refreshes the list with the given items
+		/// </summary>
+		/// <param name="items">The items to add</param>
+		public static void RefreshWith<T>(this List<T> list, IEnumerable<T> items)
+		{
+			list.Clear();
+			list.AddRange(items);
+		}
+	}
+}
